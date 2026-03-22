fc0eea41bc4d37a17d28a92a09b38abc12c1a228
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-03-22 10:47:49 +0530

    Add configuration parameters for neural network layers

---
+++ Verilog code for Implementation on FPGA/Final Code/include.v
@@ -0,0 +1,20 @@
+`define pretrained
+`define numLayers 3
+`define dataWidth 31
+`define numNeuronLayer1 128
+`define numWeightLayer1 2
+`define Layer1ActType "sigmoid"
+`define numNeuronLayer2 32
+`define numWeightLayer2 128
+`define Layer2ActType "sigmoid"
+`define numNeuronLayer3 1
+`define numWeightLayer3 32
+`define Layer3ActType "sigmoid"
+// `define numNeuronLayer4 10
+// `define numWeightLayer4 10
+// `define Layer4ActType "sigmoid"
+// `define numNeuronLayer5 10
+// `define numWeightLayer5 10
+// `define Layer5ActType "hardmax"
+`define sigmoidSize 5
+`define weightIntWidth 1
